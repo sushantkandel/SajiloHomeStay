17a68ddedf4d7e968f9b62936e8a7c4a66bf2dfa
sushantkandel1995 <peaceandray@gmail.com>
2018-07-06 18:33:42 +0545

    project initiated

---
+++ src/main/java/com/peaceandray/SajiloHomeStay/SajiloHomeStayApplication.java
@@ -0,0 +1,12 @@
+package com.peaceandray.SajiloHomeStay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajiloHomeStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SajiloHomeStayApplication.class, args);
+	}
+}
